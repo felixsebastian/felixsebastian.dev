1ce90401fcc873fa17adda7dab149cf2fe126bc0
Felix Sebastian <hi@felixsebastian.dev>
2023-01-04 19:04:25 +1100

    Create _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+const Page = (props) => (
+  <Head>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  </Head>
+);
+
+export default Page;
